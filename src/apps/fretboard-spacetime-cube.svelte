87d176bd96eb4b2de7f1bf6b1bd21e8e476a3492
fheyen <frank.heyen@outlook.de>
2024-12-11 15:04:38 +0100

    spacetimecube heatmap and examples

--- src/apps/fretboard-spacetime-cube.svelte
+++ src/apps/fretboard-spacetime-cube.svelte
@@ -8,18 +8,14 @@
     import { roundToStep } from '../lib/lib';
     import MidiInput from '../common/input-handlers/midi-input.svelte';
     import * as AFRAME from 'aframe';
+    import PcKeyboardInput from '../common/input-handlers/pc-keyboard-input.svelte';
+    import exampleStaying from '../example-recordings/fretboard-jitter/fretboard-jitter-staying-in-default.json';
+    import exampleHopping from '../example-recordings/fretboard-jitter/fretboard-jitter-hopping-between-positions.json';
+    import exampleVaried from '../example-recordings/fretboard-jitter/fretboard-jitter-strongly-varied.json';
 
     /**
      * TODO:
      * - opacity for velocity to hide noise
-     * - improve text (anchor, baseline, side, wrappixels)
-     *  - https://aframe.io/docs/1.5.0/components/text.html
-     * - allow interaction to reset notes and scale time
-     *  - https://aframe.io/docs/1.5.0/guides/building-a-basic-scene.html#event-listener-component-intermediate
-     *  - support VR controllers?
-     *      - https://aframe.io/docs/1.5.0/introduction/interactions-and-controllers.html
-     * - support hand tracking?
-     *      - https://github.com/aframevr/aframe/blob/master/docs/components/hand-tracking-controls.md
      */
 
     /**
@@ -66,7 +62,13 @@
         () => (notes = [...notes, randomNote(performance.now() / 1000)]),
         500,
     );
-    $: lastNotes = notes.filter((d) => d.time > lastTimeSeconds - pastSeconds);
+    $: lastNotes = notes.filter(
+        (d) =>
+            d.time > lastTimeSeconds - pastSeconds && d.time < lastTimeSeconds,
+        11,
+    );
+    let binnedNotes;
+    let maxValue = 1;
 
     let debugMsg = '';
 
@@ -99,6 +101,19 @@
         lastTimeSeconds = noteInSeconds;
     };
 
+    const binNotes = (notes) => {
+        binnedNotes = d3.groups(
+            notes,
+            (d) => d.string,
+            (d) => d.fret,
+        );
+        maxValue = d3.max(binnedNotes, ([string, data]) =>
+            d3.max(data, ([fret, notes]) => notes.length),
+        );
+    };
+
+    $: binNotes(lastNotes);
+
     onMount(() => {
         // https://github.com/aframevr/aframe/blob/master/docs/components/oculus-touch-controls.md#events
         // console.log(AFRAME);
@@ -128,11 +143,86 @@
         clearInterval(testInterval);
         window.location.reload();
     });
+
+    /**
+     * Used for exporting and for automatics saving
+     */
+    const getExportData = () => {
+        return {
+            timeFactor,
+            pastSeconds,
+            // data
+            notes,
+        };
+    };
+
+    /**
+     * Import data from file or example
+     */
+    const loadData = (json) => {
+        clearInterval(testInterval);
+        timeFactor = json.timeFactor ?? 1;
+        pastSeconds = json.pastSeconds ?? 60;
+        // data
+        notes = json.notes.map((d) => {
+            return { ...d, note: Midi.NOTE_NAMES[d.number % 12] };
+        });
+        // app state
+        lastTimeSeconds = notes.at(-1).time ?? 0;
+        // isDataLoaded = true;
+    };
+
+    $: opacityMap = d3.scaleLinear().domain([0, maxValue]).range([0.2, 1]);
 </script>
+
+<!-- examples -->
+<PcKeyboardInput
+    key="1"
+    keyDown="{() => {
+        loadData(exampleStaying);
+    }}"
+/>
+<PcKeyboardInput
+    key="2"
+    keyDown="{() => {
+        loadData(exampleHopping);
+    }}"
+/>
+<PcKeyboardInput
+    key="3"
+    keyDown="{() => {
+        loadData(exampleVaried);
+    }}"
+/>
+<!-- actions -->
+<PcKeyboardInput
+    key="+"
+    keyDown="{() => {
+        timeFactor += 0.1;
+    }}"
+/>
+<PcKeyboardInput
+    key="-"
+    keyDown="{() => {
+        timeFactor -= 0.1;
+    }}"
+/>
+<PcKeyboardInput
+    key="PageUp"
+    keyDown="{() => {
+        lastTimeSeconds += 1;
+    }}"
+/>
+<PcKeyboardInput
+    key="PageDown"
+    keyDown="{() => {
+        lastTimeSeconds -= 1;
+    }}"
+/>
 
 <main class="app">
     <!-- x is right, y is up, z is toward camera -->
-    <a-scene>
+    <a-scene renderer="colorManagement: true; antialias: true;">
         <!-- controllers -->
         <a-entity oculus-touch-controls="hand: left"></a-entity>
         <a-entity oculus-touch-controls="hand: right"></a-entity>
@@ -149,8 +239,8 @@
             scale=".35 .35 .35"
         ></a-entity>
         <a-entity
-            text="value: Connect a MIDI guitar and start playing. Notes are positioned based on their string (forward), fret (right), and time (up). They are colored by string and labelled with note name and fret number.\n\nRandom notes are shown until you play.\n\nGo back in your browser to return to the main page.; color: #666; width: 5"
-            position="-2 1.5 -3"
+            text="value: Connect a MIDI guitar and start playing. Notes are positioned based on their string (forward), fret (right), and time (up). They are colored by string and labelled with note name and fret number.\n\nRandom notes are shown until you play.\n\nPress 1, 2, 3 for examples, +/- to change the time scale,page up/down to scroll throguh time.\n\nGo back in your browser to return to the main page.; color: #666; width: 5"
+            position="-2 1.2 -3"
             scale=".25 .25 .25"
         ></a-entity>
         <!-- text for debugging -->
@@ -161,7 +251,7 @@
         ></a-entity>
         <!-- visualization container -->
         <a-box
-            position="-1 .5 -3"
+            position="-1 0 -3"
             rotation="0 0 0"
             scale=".1 .1 .1"
             visible="true"
@@ -240,17 +330,31 @@
                 <a-box
                     position="{`${note.fret - 0.3} ${scaleTime(note.time)} ${note.string - 5}`}"
                     color="{stringColors[stringCount - note.string - 1]}"
-                    opacity="0.5"
-                    width="0.4"
-                    height="0.4"
-                    depth="0.4"
+                    opacity="{note.velocity / 127}"
+                    width="0.3"
+                    height="0.3"
+                    depth="0.3"
                 >
                     <a-entity
                         text="value: {note.note}\n{note.fret}; color: #666"
-                        position="2.35 0 0.201"
+                        position="2.35 0 0.25"
                         scale="5 5 5"
+                        opacity="{note.velocity / 127}"
                     ></a-entity>
                 </a-box>
+            {/each}
+            <!-- heatmap -->
+            {#each binnedNotes as [stringPos, stringNotes]}
+                {#each stringNotes as [fretPos, notes]}
+                    <a-box
+                        position="{`${fretPos - 0.3} 0 ${stringPos - 5}`}"
+                        color="{stringColors[stringCount - stringPos - 1]}"
+                        width="0.4"
+                        height="0.1"
+                        depth="0.4"
+                        opacity="{opacityMap(notes.length)}"
+                    ></a-box>
+                {/each}
             {/each}
         </a-box>
     </a-scene>
